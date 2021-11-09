ec73a2447bbdaea61b05033f3814660a8dc1254a
Teodor Banea <teodor.toe@gmail.com>
2021-11-09 22:06:45 +0200

    Second test working update

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 import time
-
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LocationSearch(unittest.TestCase):
 
     def setUp(self):
         PATH = "/usr/local/bin/chromedriver"
         self.driver = webdriver.Chrome(PATH)
+        self.driver.get("https://www.bayut.com/")
 
     def test_search_location(self):
         driver = self.driver
-        driver.get("https://www.bayut.com/")
 
         cursor = driver.find_element(By.XPATH ,"//div[@name='purpose']//span[@class='ef5cccac']")
         cursor.click()
@@ -31,11 +33,38 @@ class LocationSearch(unittest.TestCase):
         cursor.click()
         time.sleep(1)
 
-        json_location = driver.find_element(By.XPATH,"//script[contains(text(),'{\"@context\":\"https://schema.org\",\"@type\":[\"Place\",')]").get_attribute('innerHTML')
+        json_location = driver.find_element(By.XPATH, "//script[contains(text(),'{\"@context\":\"https://schema.org\",\"@type\":[\"Place\",')]").get_attribute('innerHTML')
         parsed_json = json.loads(json_location)
         for element in parsed_json['itemListElement']:
-            #print(element['mainEntity']['address']['addressLocality'])
             self.assertEqual(element['mainEntity']['address']['addressLocality'], 'Dubai Marina')
+
+    def test_valid_links_bayut(self):
+        driver = self.driver
+        driver.maximize_window()
+        links = [link.get_attribute("href") for link in driver.find_elements(By.XPATH, "//body//div//div[@aria-label='Popular properties']//div//div[2]//div[1]//div[1]//div[1]//div[1]//div[1]//div[2]//div[1]//ul[1]//li//a")]
+        for link in links:
+            driver.get(link)
+            time.sleep(2)
+            print("primu print din for " + link)
+            print(link)
+            time.sleep(1)
+
+            purpose = driver.find_element(By.XPATH,"(//span[contains(text(),'Rent')])[2]")
+            self.assertEqual(purpose.text, 'Rent')
+            print("a trecut primu assert")
+
+            location_aux = " ".join(link.split("/")[-2].split("-")).lower()
+
+            location = driver.find_element(By.XPATH,"//span[@aria-label='Filter label']")
+
+            self.assertIn(location_aux, location.text.replace("(","").replace(")","").lower())
+            print("a trecut al doilea assert")
+
+            time.sleep(2)
+
+            print("a ajuns si la sfarsitul for-ului")
+            time.sleep(1)
+
 
     def tearDown(self):
         self.driver.close()
